22b90ee68933dc129e629ba1a7fb76f87895c49c
Laurens Verspeek <l.verspeek@gmail.com>
2021-09-27 18:21:24 +0200

    show account info on profile page

--- pages/profile.vue
+++ pages/profile.vue
@@ -2,17 +2,49 @@
   <section class="section">
     <div class="container">
       <div class="text-center">
-        <h4 class="title">
-          Welcome
+        <h4 class="title is-spaced">
+          Your Effect Account
         </h4>
-        <div class="subtitle">
-          <a
-            :href="($auth.user.blockchain === 'bsc' ? $blockchain.bsc.explorer : $blockchain.eos.explorer) + '/address/'+ $auth.user.accountName"
-            target="_blank"
-            class="blockchain-address"
-          >{{ $auth.user.accountName }}</a>
-          {{ $auth.user }}
+        <div class="block">
+          <div class="has-text-weight-bold is-size-6">
+            {{ $auth.user.blockchain === 'bsc' ? 'BSC Address' : 'EOS Account Name' }}:
+          </div>
+          <div class="subtitle">
+            <a
+              :href="($auth.user.blockchain === 'bsc' ? $blockchain.bsc.explorer : $blockchain.eos.explorer) + '/address/'+ $auth.user.accountName"
+              target="_blank"
+              class="blockchain-address"
+            >{{ $auth.user.accountName }}</a><span v-if="$blockchain.account.permission">@{{ $blockchain.account.permission }}</span>
+          </div>
         </div>
+        <div class="block">
+          <div class="has-text-weight-bold is-size-6">
+            Effect Account ID:
+          </div>
+          <div class="subtitle">
+            {{ $auth.user.vAccountRows[0].id }}
+          </div>
+        </div>
+        <div class="block">
+          <div class="has-text-weight-bold is-size-6">
+            Wallet Connection:
+          </div>
+          <div class="subtitle">
+            {{ $auth.user.provider }}@{{ $auth.user.blockchain }}
+          </div>
+        </div>
+        <div class="block">
+          <div class="has-text-weight-bold is-size-6">
+            Blockchain:
+          </div>
+          <div class="subtitle">
+            <a
+              :href="`${$blockchain.eos.explorer}/account/${$blockchain.sdk.account.config.ACCOUNT_CONTRACT}?loadContract=true&tab=Tables&table=account&account=${$blockchain.sdk.account.config.ACCOUNT_CONTRACT}&scope=${$blockchain.sdk.account.config.ACCOUNT_CONTRACT}&limit=1&lower_bound=${$auth.user.vAccountRows[0].id}&upper_bound=${$auth.user.vAccountRows[0].id}`"
+              target="_blank"
+            >View on explorer</a>
+          </div>
+        </div>
+        {{ $auth.user }}
         <a class="button is-danger" @click="logout">Logout</a>
         <br><br>
       </div>
