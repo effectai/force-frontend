254ff9e46f1a6a32b458d8d6f520f9d9cb5c8895
Noman Jabbar <noman.jabbar@hva.nl>
2021-10-01 14:47:17 +0200

    add block explorer link

--- pages/campaigns/_id/index.vue
+++ pages/campaigns/_id/index.vue
@@ -72,6 +72,11 @@
               <br>
               <a target="_blank" :href="'https://ipfs.effect.ai/ipfs/' + campaign.content_hash">{{ campaign.content_hash.substring(0, 22) }}...</a>
             </div>
+            <div class="block">
+              <b>Block Explorer</b>
+              <br>
+              <a target="_blank" :href="`https://kylin.bloks.io/account/propsonkylin?loadContract=true&tab=Tables&table=proposal&account=propsonkylin&scope=propsonkylin&limit=1&lower_bound=${id}&upper_bound=${id}`">View on Block Explorer</a>
+            </div>
           </div>
         </div>
       </div>
