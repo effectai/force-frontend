bf5729a548bfd2453bf7f93a9b4201ce7bac998c
Noman Jabbar <nomanjabbar1999@gmail.com>
2021-10-11 20:39:47 +0200

    improve the already improved category filters, remove redundant code, show active filters when clicked, add all button

--- components/CategoryFilters.vue
+++ components/CategoryFilters.vue
@@ -1,30 +1,23 @@
 <template>
   <div class="columns">
-    <div class="column">
-      <a href="#" class="card is-flat is-hoverable dapp-dao" @mouseover="effect_dapps.dao.hover = true" @mouseleave="effect_dapps.dao.hover = false" @click.prevent="onClick('translation')">
+    <div v-for="(dapp, index) in effect_dapps" :key="dapp.value" class="column">
+      <a
+        href="#"
+        :class="['dapp-' + index, filter === dapp.value ? 'is-active' : null]"
+        class="card is-flat is-hoverable"
+        @mouseover="dapp.hover = true"
+        @mouseleave="dapp.hover = false"
+        @click.prevent="onClick(dapp.value)"
+      >
         <div class="card-image has-text-centered">
-          <img class="dapp-logo block mt-2" :src="effect_dapps.dao.hover ? effect_dapps.dao.whiteUrl : effect_dapps.dao.normalUrl" alt="Image">
+          <img class="dapp-logo block mt-2" :src="dapp.hover || filter === dapp.value ? dapp.whiteUrl : dapp.normalUrl" alt="Image">
         </div>
       </a>
     </div>
-    <div class="column">
-      <a href="#" class="card is-flat is-hoverable dapp-socials" @mouseover="effect_dapps.socials.hover = true" @mouseleave="effect_dapps.socials.hover = false" @click.prevent="onClick('image_classification')">
-        <div class="card-image has-text-centered">
-          <img class="dapp-logo block mt-2" :src="effect_dapps.socials.hover ? effect_dapps.socials.whiteUrl : effect_dapps.socials.normalUrl" alt="Image">
-        </div>
-      </a>
-    </div>
-    <div class="column">
-      <a href="#" class="card is-flat is-hoverable dapp-captions" @mouseover="effect_dapps.captions.hover = true" @mouseleave="effect_dapps.captions.hover = false" @click.prevent="onClick('text_classification')">
-        <div class="card-image has-text-centered">
-          <img class="dapp-logo block mt-2" :src="effect_dapps.captions.hover ? effect_dapps.captions.whiteUrl : effect_dapps.captions.normalUrl" alt="Image">
-        </div>
-      </a>
-    </div>
-    <div class="column">
-      <a href="#" class="card is-flat is-hoverable dapp-translate" @mouseover="effect_dapps.translate.hover = true" @mouseleave="effect_dapps.translate.hover = false" @click.prevent="onClick('video_classification')">
-        <div class="card-image has-text-centered">
-          <img class="dapp-logo block mt-2" :src="effect_dapps.translate.hover ? effect_dapps.translate.whiteUrl : effect_dapps.translate.normalUrl" alt="Image">
+    <div class="column is-2">
+      <a href="#" class="card is-flat is-hoverable" @click.prevent="onClick(null)">
+        <div class="card-content has-text-centered">
+          <h4 class="is-size-5"><b>Show All</b></h4>
         </div>
       </a>
     </div>
@@ -35,24 +28,29 @@ export default {
   name: 'CategoryFilters',
   data () {
     return {
+      filter: null,
       effect_dapps: {
         dao: {
           hover: false,
+          value: 'translation',
           normalUrl: 'https://effect.network/_nuxt/img/effect-dao_h100.62b6528.png',
           whiteUrl: 'https://effect.network/_nuxt/img/effect-dao_white_h100.8128b59.png'
         },
         socials: {
           hover: false,
+          value: 'image_classification',
           normalUrl: 'https://effect.network/_nuxt/img/effect-socials_h100.4f0f9f8.png',
           whiteUrl: 'https://effect.network/_nuxt/img/effect-socials_white_h100.aaa70fd.png'
         },
         translate: {
           hover: false,
+          value: 'video_classification',
           normalUrl: 'https://effect.network/_nuxt/img/effect-translate_h100.61861ae.png',
           whiteUrl: 'https://effect.network/_nuxt/img/effect-translate_white_h100.d698b51.png'
         },
         captions: {
           hover: false,
+          value: 'text_classification',
           normalUrl: 'https://effect.network/_nuxt/img/effect-captions_h100.2b22d0c.png',
           whiteUrl: 'https://effect.network/_nuxt/img/effect-captions_white_h100.d746459.png'
         }
@@ -61,13 +59,14 @@ export default {
   },
   methods: {
     onClick (category) {
-      this.$emit('clicked', category)
+      this.filter = category
+      this.$emit('clicked', this.filter)
     }
   }
 }
 </script>
 <style lang="scss" scoped>
-  a.card:hover {
+  a.card:hover, a.card.is-active {
     &.dapp-dao div.card-image {
       background: #d7ac00;
     }
