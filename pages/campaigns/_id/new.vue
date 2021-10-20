849d22435e06f91ec05fb233e5a085b497774eb6
Laurens Verspeek <l.verspeek@gmail.com>
2021-10-20 17:49:23 +0200

    add tasks form based on placeholders for new batch

--- pages/campaigns/_id/new.vue
+++ pages/campaigns/_id/new.vue
@@ -20,77 +20,145 @@
           </li>
         </ul>
       </nav>
-      <form @submit.prevent="uploadBatch">
-        <div class="field">
-          <label class="label">Repetitions</label>
-          <div class="control">
-            <input v-model="repetitions" class="input" type="number" min="0" required>
+      <div v-if="campaignLoading">
+        Campaign loading..
+      </div>
+      <div v-else-if="!campaign">
+        Could not retrieve campaign
+      </div>
+      <div v-else>
+        <div class="title">
+          Tasks
+        </div>
+        <div>
+          <div v-for="(task,index) in tasks" :key="index">
+            #{{ index }}: {{ task }} <span class="has-text-danger-dark is-size-5 has-text-weight-bold" @click="tasks.splice(index, 1)">x</span>
+          </div>
+          <div v-if="!tasks.length">
+            No tasks..
           </div>
         </div>
-        <div class="field">
-          <label class="label">CSV?</label>
-          <div class="control">
-            <textarea v-model="tasks" class="textarea" required placeholder="[{}]" />
+        <form @submit.prevent="createTask">
+          <div class="field">
+            <div v-for="placeholder in placeholders" :key="placeholder" class="task-placeholder-value">
+              <label class="label">{{ placeholder }}</label>
+              <div class="control">
+                <input v-model="task[placeholder]" type="text" class="input">
+              </div>
+              <div class="control">
+                <button type="submit" class="button">
+                  Create Task
+                </button>
+              </div>
+            </div>
           </div>
-        </div>
-        <div class="field is-grouped">
-          <div class="control">
-            <button type="submit" class="button is-link">
-              Submit
-            </button>
+        </form>
+        <form @submit.prevent="uploadBatch">
+          <div class="field">
+            <label class="label">Repetitions</label>
+            <div class="control">
+              <input v-model="repetitions" class="input" type="number" min="0" required>
+            </div>
           </div>
-          <div class="control">
-            <button class="button is-link is-light">
-              Cancel
-            </button>
+          <div class="field is-grouped">
+            <div class="control">
+              <button type="submit" class="button is-link" :disabled="!tasks.length">
+                Submit
+              </button>
+            </div>
+            <div class="control">
+              <button class="button is-link is-light">
+                Cancel
+              </button>
+            </div>
           </div>
-        </div>
-      </form>
+        </form>
+      </div>
     </div>
   </section>
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
+import { mapState, mapGetters } from 'vuex'
+
+function getMatches (string, regex, index) {
+  index || (index = 1) // default to the first capturing group
+  const matches = []
+  let match
+  while ((match = regex.exec(string))) {
+    matches.push(match[index])
+  }
+  return matches
+}
 
 export default {
   middleware: ['auth'],
   data () {
     return {
       campaignId: parseInt(this.$route.params.id),
-      batchId: null,
       repetitions: null,
-      tasks: null
+      task: {},
+      tasks: [],
+      placeholders: null,
+      campaign: null
     }
   },
   computed: {
+    ...mapState({
+      campaigns: state => state.campaign.campaigns,
+      campaignLoading: state => state.campaign.loading
+    }),
     ...mapGetters({
       batchByCampaignId: 'campaign/batchByCampaignId'
     })
   },
   created () {
-    this.getBatches()
+    this.getCampaign()
   },
   methods: {
+    createTask () {
+      this.tasks.push(this.task)
+      this.task = this.getEmptyTask(this.placeholders)
+    },
+    getEmptyTask (placeholders) {
+      const emptyTask = {}
+      placeholders.forEach((placeholder) => {
+        emptyTask[placeholder] = ''
+      })
+      return emptyTask
+    },
+    getPlaceholders (template) {
+      const placeholders = getMatches(
+        template,
+        /\$\{\s?(\w+)\s?\|?\s?(\w*)\s?\}/g
+      )
+      const unique = [...new Set(placeholders)]
+      this.placeholders = unique
+    },
+    async getCampaign () {
+      await this.$store.dispatch('campaign/getCampaign', this.campaignId)
+      this.campaign = this.campaigns.find(c => c.id === this.campaignId)
+      this.getPlaceholders(this.campaign.info.template)
+      this.task = this.getEmptyTask(this.placeholders)
+    },
     async uploadBatch () {
       try {
-        this.tasks = JSON.parse(this.tasks)
         const content = {
           tasks: this.tasks
         }
-        const result = await this.$blockchain.createBatch(this.campaignId, this.batchId, content, this.repetitions)
+        const batchId = await this.getNewBatchId()
+        const result = await this.$blockchain.createBatch(this.campaignId, batchId, content, this.repetitions)
         this.$store.dispatch('transaction/addTransaction', result)
         this.$router.push('/campaigns/' + this.campaignId)
       } catch (e) {
         this.$blockchain.handleError(e)
       }
     },
-    async getBatches () {
+    async getNewBatchId () {
       await this.$store.dispatch('campaign/getBatches')
 
       if (this.batchByCampaignId(this.campaignId) !== null) {
-        const batchesLength = this.batchByCampaignId(this.campaignId).length
-        this.batchId = batchesLength
+        return this.batchByCampaignId(this.campaignId).length
       }
     }
   }
