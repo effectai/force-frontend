35e38741b9ea1a63f383cb394a4f3bb4d076f934
Laurens Verspeek <l.verspeek@gmail.com>
2022-02-11 12:37:09 +0100

    dynamically load default templates from github api

--- pages/campaigns/templates.vue
+++ pages/campaigns/templates.vue
@@ -8,11 +8,20 @@
         Select a customizable template to start a new campaign
       </h2>
       <div class="columns">
-        <div class="column is-2">
+        <div class="column is-3">
           <div class="panel">
             <p class="panel-heading">
               Templates
             </p>
+            <div v-if="loading">
+              Loading..
+            </div>
+            <div v-else-if="!templates">
+              Could not retrieve default templates
+            </div>
+            <div v-else-if="!templates.length">
+              No default templates found
+            </div>
             <a v-for="template in templates" :key="template.url" class="panel-block" :class="{'is-active': selectedTemplate === template}" @click="selectedTemplate = template">
               <span class="panel-icon">
                 ›
@@ -21,7 +30,7 @@
             </a>
           </div>
         </div>
-        <div class="column is-10">
+        <div class="column is-9">
           <h2 class="panel-heading">
             Preview
           </h2>
@@ -36,7 +45,7 @@
         </nuxt-link>
       </div>
       <div class="control">
-        <nuxt-link :to="`/campaigns/new?templateUrl=${selectedTemplate.url}&placeholders=${JSON.stringify(selectedTemplate.placeholders)}`" class="button is-primary is-wide">
+        <nuxt-link v-if="selectedTemplate" :disabled="loading" :to="`/campaigns/new?templateUrl=${selectedTemplate.url}&placeholders=${JSON.stringify(selectedTemplate.placeholders)}`" class="button is-primary is-wide">
           Use Template
         </nuxt-link>
       </div>
@@ -79,12 +88,20 @@ export default {
       ]
     }
   },
-
   watch: {
     async selectedTemplate (template) {
       this.loading = true
       try {
         const response = await this.$axios.get(template.url)
+        if (typeof template.placeholders === 'string') {
+          try {
+            const response2 = await this.$axios.get(template.placeholders)
+            template.placeholders = response2.data
+          } catch (e) {
+            template.placeholders = {}
+            console.error('could not retrieve example task', e)
+          }
+        }
         this.previewTemplate = this.renderTemplate(response.data, template.placeholders)
       } catch (e) {
         console.error(e)
@@ -94,10 +111,34 @@ export default {
   },
 
   created () {
-    this.selectedTemplate = this.templates[0]
+    this.retrieveTemplates()
   },
 
   methods: {
+    async retrieveTemplates () {
+      this.loading = true
+      try {
+        const response = await fetch('https://api.github.com/repos/effectai/effect-force-templates/git/trees/master')
+        const templatesTree = (await response.json()).tree.find(t => t.path === 'templates')
+        if (templatesTree) {
+          const response2 = await fetch(templatesTree.url)
+          const templatesTrees = (await response2.json()).tree
+          const templates = []
+          templatesTrees.forEach((template) => {
+            templates.push({
+              name: template.path,
+              url: `https://raw.githubusercontent.com/effectai/effect-force-templates/master/templates/${template.path}/template.html`,
+              placeholders: `https://raw.githubusercontent.com/effectai/effect-force-templates/master/templates/${template.path}/example_task.json`
+            })
+          })
+          this.templates = templates
+          this.selectedTemplate = this.templates[0]
+        }
+      } catch (e) {
+        console.error(e)
+      }
+      this.loading = false
+    },
     renderTemplate (template, placeholders = {}, options = {}) {
       return new Template(template, placeholders, options).render()
     }
