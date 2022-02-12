89c021034412ab1444a5e81d1218477ecf06e74e
David Britt <david@effect.ai>
2022-02-12 17:49:12 +0100

    remove logs

--- store/pendingPayout.js
+++ store/pendingPayout.js
@@ -8,8 +8,8 @@ export default {
       state.loading = loading
     },
     SET_PENDING_PAYOUT (state, payouts) {
-      console.log('SET_PENDING_PAYOUT, state', state)
-      console.log('SET_PENDING_PAYOUT, payouts', payouts)
+      // console.log('SET_PENDING_PAYOUT, state', state)
+      // console.log('SET_PENDING_PAYOUT, payouts', payouts)
 
       if (state.pendingPayouts === null) {
         state.pendingPayouts = {}
@@ -25,12 +25,10 @@ export default {
   actions: {
     async loadPendingPayouts (context) {
       context.commit('SET_LOADING', true)
-      console.log('loadPendingPayouts, context', context)
+      // console.log('loadPendingPayouts, context', context)
       const data = await this.$blockchain.getPendingPayouts().catch(console.error)
-      console.log('loadPendingPayouts, data', data)
-      // const rows = [...data.rows]
-
-      console.log('Live from pendingpayout.js vuex store.', data)
+      // console.log('loadPendingPayouts, data', data)
+      // console.log('Live from pendingpayout.js vuex store.', data)
       context.commit({ type: 'SET_PENDING_PAYOUT', payouts: data })
       // context.commit('SET_PENDING_PAYOUT', data.rows)
       context.commit('SET_LOADING', false)
