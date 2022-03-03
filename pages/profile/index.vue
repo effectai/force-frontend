c776618bff387e5198336e6edffda7f82c360369
David Britt <david@effect.ai>
2022-03-03 12:32:37 +0100

    Remove console logs and update userinfo immediately after payout

--- pages/profile/index.vue
+++ pages/profile/index.vue
@@ -285,6 +285,7 @@ export default {
         const result = await this.$blockchain.payout()
         this.$store.dispatch('transaction/addTransaction', result)
         this.$store.dispatch('pendingPayout/loadPendingPayouts')
+        this.$blockchain.updateUserInfo()
         this.transactionUrl = process.env.NUXT_ENV_EOS_EXPLORER_URL + '/transaction/' + result.transaction_id
         this.successTitle = 'Payout Completed'
         this.successMessage = 'All your available pending payouts have been completed and are added to your Effect account'
