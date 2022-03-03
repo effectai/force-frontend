6e7fdb2176fcadf2c3c50a7bac78c5007a9dbc40
David Britt <david@effect.ai>
2022-03-03 12:06:02 +0100

    Remove redundant column, and fix buttons for mobile

--- store/index.js
+++ store/index.js
@@ -1,4 +1,5 @@
 import VuexPersistence from 'vuex-persist'
+
 const vuexLocal = new VuexPersistence({
   storage: process.client ? window.localStorage : null,
   modules: ['transaction', 'ipfs', 'template', 'view']
