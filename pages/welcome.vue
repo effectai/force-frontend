568265ed4ce9485ed1ae98a950b13576944fe3ba
Laurens Verspeek <l.verspeek@gmail.com>
2022-03-14 15:14:47 +0100

    implement design for welcome landing page

--- pages/welcome.vue
+++ pages/welcome.vue
@@ -1,125 +1,206 @@
 <template>
-  <div>
-    <div class="is-flex is-flex-wrap-wrap split" style="position: relative">
-      <section class="hero is-secondary">
-        <div class="hero-body">
-          <div class="py-6">
-            <div class="py-6 has-text-centered">
-              <font-awesome-icon style="font-size: 80px" icon="fa-solid fa-coins" />
+  <div class="split">
+    <section class="section">
+      <div class="container">
+        <div class="columns is-vcentered">
+          <div class="column is-6 bg-hover bg-hover-left">
+            <div class="columns">
+              <div class="column is-8-widescreen is-10-desktop is-11-tablet">
+                <img src="~assets/img/icons/coins.png" class="show-on-hover">
+                <img src="~assets/img/icons/coins.png" class="hide-on-hover">
+                <h1 class="title is-1">
+                  Earn Crypto
+                </h1>
+                <h1 class="title is-4">
+                  Complete tasks and earn the EFX cryptocurrency
+                </h1>
+                <p class="has-text-aligned block">
+                  Effect Force is the decentralized framework for the Future-of-Work. You can now work from anywhere at any time from any device. Simply create an Effect Account and sign into Effect Force to start earning great pay in the cryptocurrency $EFX.
+                </p>
+                <p class="block">
+                  <nuxt-link class="button is-primary is-outlined is-medium" to="/login">
+                    Join the WorkForce
+                  </nuxt-link>
+                </p>
+              </div>
             </div>
-            <h1 class="title">
-              Earn Crypto
-            </h1>
-            <h2 class="subtitle">
-              Complete tasks and earn the EFX cryptocurrency
-            </h2>
-            <p class="has-text-aligned block">
-              Effect Force is the decentralized framework for the Future-of-Work. Currently there are over 10,000 people earning great pay in the cryptocurrency $EFX. You can now work from anywhere at any time from any device. Simply create an Effect Account and sign into Effect Force to start your future today.
-            </p>
-            <p class="block">
-              <nuxt-link class="button is-primary" to="/login">
-                Join the WorkForce
-              </nuxt-link>
-            </p>
           </div>
-        </div>
-      </section>
-      <section class="hero is-primary">
-        <div class="hero-body">
-          <div class="py-6">
-            <div class="py-6 has-text-centered">
-              <font-awesome-icon style="font-size: 80px" icon="fa-solid fa-laptop-code" />
+          <div class="bg-left" />
+          <div class="column is-6 bg-hover bg-hover-right">
+            <div class="columns">
+              <div class="column is-4-widescreen is-2-desktop is-1-tablet" />
+              <div class="column is-8-widescreen is-10-desktop is-11-tablet">
+                <img src="~assets/img/icons/create.png" class="show-on-hover">
+                <img src="~assets/img/icons/create.png" class="hide-on-hover">
+                <h1 class="title is-1">
+                  Create Task
+                </h1>
+                <h1 class="title is-4">
+                  Access a global, on-demand, 24x7 scalable WorkForce
+                </h1>
+                <p class="has-text-aligned block">
+                  Tap into a scalable WorkForce of over 10,000 people to complete tasks for you. Choose one of the default task templates to get started with text analysis, document transcription, image annotations, sentitment analyis and many more!
+                </p>
+                <p class="block">
+                  <nuxt-link class="button is-primary is-outlined is-medium" to="/campaigns/templates">
+                    Create a task
+                  </nuxt-link>
+                </p>
+              </div>
             </div>
-            <h1 class="title">
-              Create Tasks
-            </h1>
-            <h2 class="subtitle">
-              Access a global, on-demand, 24x7 scalable WorkForce
-            </h2>
-            <p class="has-text-aligned block">
-              Effect Force is the decentralized framework for the Future-of-Work. Currently there are over 10,000 people earning great pay in the cryptocurrency $EFX. You can now work from anywhere at any time from any device. Simply create an Effect Account and sign into Effect Force to start your future today.
-            </p>
-            <p class="block">
-              <nuxt-link class="button is-secondary" to="/campaigns/templates">
-                Create a task
-              </nuxt-link>
-            </p>
           </div>
-        </div>
-      </section>
-      <div class="hero is-primary dynamic-title">
-        <div class="container has-text-centered">
-          <h1 class="title">
-            Get Started with Effect Force
-          </h1>
+          <div class="bg-right" />
         </div>
       </div>
       <div class="or">
         OR
       </div>
-    </div>
+    </section>
   </div>
 </template>
 
 <style lang="scss" scoped>
+.container {
+  position: unset;
+  > .columns {
+    height: calc(100vh - 350px);
+  }
+}
+
 @import "bulma/sass/utilities/mixins";
-    .dynamic-title{
-        width: 100%;
-        position: absolute;
-        top: 0;
-        transition: 0.2s;
-    }
-    .split section.is-primary:hover ~ .dynamic-title {
-        background-color: $primary;
-    }
-    .split section.is-accent:hover ~ .dynamic-title {
-        background-color: $accent;
-        color: black;
-    }
-    .split section.is-secondary:hover ~ .dynamic-title {
-        background-color: $secondary;
-    }
-    .split section {
-      width: 50%;
-      opacity: 0.7;
-      transition: 0.2s;
-      min-height: calc(100vh - 108px);
-      &:hover {
-          opacity: 1;
+
+.bg-left, .bg-right {
+   position: absolute;
+    background: #FAFDFF;
+    height: 100vh;
+    width: 50%;
+    left: 0;
+    top:0;
+    z-index:0;
+    &:hover {
+      &:after {
+        height: 100vh;
+        background: $primary;
       }
     }
+
+    &:after {
+      content: "";
+      display: block;
+      position: absolute;
+      background: transparent;
+      height: 0;
+      width: 100%;
+      left: 0;
+      top:0;
+      transition: 0.3s;
+      z-index:0;
+    }
+}
+.bg-hover {
+    z-index: 1;
+    &:before {
+      content: "";
+      display: block;
+      background: transparent;
+      position: absolute;
+      left: 0;
+      top: 0;
+      width: 50%;
+      height: 100vh;
+    }
+    .button {
+      transition: 0.3s;
+    }
+}
+.bg-left, .bg-hover-left:before {
+  left: 0;
+}
+.bg-right, .bg-hover-right:before {
+  left: 50%
+}
+.hide-on-hover {
+  display: block;
+}
+.show-on-hover {
+  display: none;
+}
+.bg-left:hover {
+      ~ .bg-hover-left {
+      color: white;
+      .title, .subtitle {
+        color: white;
+      }
+      .show-on-hover {
+        display: block;
+      }
+      .hide-on-hover {
+        display: none;
+      }
+      .button {
+        border-color: $accent !important;
+        background: $accent !important;
+        color: black !important;
+      }
+    }
+}
+.bg-hover-right {
+    &:hover {
+      + .bg-right:after {
+        height: 100vh;
+        background: $primary;
+      }
+  }
+}
+.bg-hover-left {
+    &:hover {
+      + .bg-left:after {
+        height: 100vh;
+        background: $primary;
+      }
+  }
+}
+
   .or {
     position: absolute;
-    top: calc(50% - 28px);
+    top: calc(50% - 35px);
     font-size: 35px;
     font-weight: 900;
-    color: white;
-    left: calc(50% - 26px);
-    &:before {
-        width: 3px;
-        height: 40px;
-        content: "";
+    left: calc(50% - 45px);
+    width: 90px;
+height: 70px;
+padding-top: 12px;
+text-align: center;
+font-size: 25px;
+
+background: #FAFDFF;
+border: 1px solid #BFCDF5;
+box-sizing: border-box;
+box-shadow: 0px 4px 20px rgba(16, 29, 86, 0.2);
+border-radius: 20px;
+  }
+  @include desktop {
+    .bg-hover:hover {
+      color: white;
+      .title, .subtitle {
+        color: white;
+      }
+      .show-on-hover {
         display: block;
-        left: 24px;
-        bottom: 50px;
-        background: white;
-        position: absolute;
-    }
-    &:after {
-        width: 3px;
-        height: 40px;
-        content: "";
-        display: block;
-        left: 24px;
-        top: 50px;
-        background: white;
-        position: absolute;
+      }
+      .hide-on-hover {
+        display: none;
+      }
+      .button {
+        border-color: $accent !important;
+        background: $accent !important;
+        color: black !important;
+      }
     }
   }
   @include touch {
-    .split section {
-        width: 100%;
-        opacity: 1;
+    .bg-left, .bg-right, .or {
+      display: none;
     }
   }
 </style>
