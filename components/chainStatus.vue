f914ef136573eafdb7eda12499f25c49eca995bf
Laurens Verspeek <l.verspeek@gmail.com>
2022-03-14 18:00:16 +0100

    don't spam rpc node with calls

--- components/chainStatus.vue
+++ components/chainStatus.vue
@@ -1,37 +1,38 @@
 <template>
   <div class="columns is-centered is-multiline">
     <div class="column is-third">
-        <span>
-          <strong>BSC-Block:</strong>
-        </span>
-        <span>
-          <a
-            v-if="bscBlockHeight !== null"
-            :href="`${$blockchain.bsc.explorer}/block/${bscBlockHeight - 5}`"
-            target="_blank"
-            rel="noopener noreferrer">
-            {{ bscBlockHeight }}
-          </a>
-          <span v-else>...</span>
-        </span>
+      <span>
+        <strong>BSC-Block:</strong>
+      </span>
+      <span>
+        <a
+          v-if="bscBlockHeight !== null"
+          :href="`${$blockchain.bsc.explorer}/block/${bscBlockHeight - 5}`"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          {{ bscBlockHeight }}
+        </a>
+        <span v-else>...</span>
+      </span>
     </div>
     <div class="column is-third">
-        <span>
-          <strong>EOS-Block:</strong>
-        </span>
-        <span>
-          <a
-            v-if="eosBlockHeight !== null"
-            :href="`${$blockchain.eos.explorer}/block/${eosBlockHeight}`"
-            target="_blank"
-            rel="noopener noreferrer">
-            {{ eosBlockHeight }}
-          </a>
-          <span v-else>...</span>
-        </span>
+      <span>
+        <strong>EOS-Block:</strong>
+      </span>
+      <span>
+        <a
+          v-if="eosBlockHeight !== null"
+          :href="`${$blockchain.eos.explorer}/block/${eosBlockHeight}`"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          {{ eosBlockHeight }}
+        </a>
+        <span v-else>...</span>
+      </span>
     </div>
   </div>
-
 </template>
 
 <script>
@@ -45,15 +46,15 @@ export default {
       bscBlockHeight: null
     }
   },
+  computed: {
+  },
   created () {
     if (!this.updateChainInterval) {
-      this.updateChainInterval = setInterval(() => this.updateChainHeight(), 1000)
+      this.updateChainInterval = setInterval(() => this.updateChainHeight(), 60000)
     }
   },
   beforeDestroy () {
     clearInterval(this.updateChainInterval)
-  },
-  computed: {
   },
   methods: {
     async updateChainHeight () {
