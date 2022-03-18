640a1df5489a01f434dc48fc2eaf7c46e6e4cadf
David Britt <david@effect.ai>
2022-03-18 15:26:04 +0100

    Fix formatting footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -2,18 +2,20 @@
   <footer class="footer">
     <div class="level">
       <div class="level-item level-left">
-        <a href="https://github.com/effectai/force-frontend-new/issues/new" target="_blank" rel="noopener noreferrer"><font-awesome-icon class="mx-2" icon="fa-solid fa-code-pull-request" size="2x"/></a>
+        <a href="https://github.com/effectai/force-frontend-new/issues/new" target="_blank" rel="noopener noreferrer"><font-awesome-icon class="mx-2" icon="fa-solid fa-code-pull-request" size="xl" style="color: grey"/></a>
+        <a href="https://github.com/effectai" target="_blank" rel="noopener noreferrer"><font-awesome-icon class="mx-2" icon="fa-brands fa-github" size="xl" style="color: grey"/></a>
+        <a href="https://discord.gg/3GrrfJrdyN" target="_blank" rel="noopener noreferrer"><font-awesome-icon class="mx-2" icon="fa-brands fa-discord" size="xl" style="color: grey"/></a>
       </div>
       <div class="level-item has-text-centered">
-        <a href="https://effect.network" target="_blank" rel="noopener noreferrer"><font-awesome-icon class="mx-2" icon="fa-solid fa-house" size="2x"/></a>
-        <a href="https://github.com/effectai" target="_blank" rel="noopener noreferrer"><font-awesome-icon class="mx-2" icon="fa-brands fa-github" size="2x"/></a>
-        <a href="https://discord.gg/3GrrfJrdyN" target="_blank" rel="noopener noreferrer"><font-awesome-icon class="mx-2" icon="fa-brands fa-discord" size="2x"/></a>
+        <!-- <a href="https://github.com/effectai" target="_blank" rel="noopener noreferrer"><font-awesome-icon class="mx-2" icon="fa-brands fa-github" size="xl" style="color: grey"/></a>
+        <a href="https://discord.gg/3GrrfJrdyN" target="_blank" rel="noopener noreferrer"><font-awesome-icon class="mx-2" icon="fa-brands fa-discord" size="xl" style="color: grey"/></a> -->
         <!-- We can add these in the future -->
-        <!-- <a href="https://twitter.com/effectaix" target="_blank" rel="noopener noreferrer"><font-awesome-icon icon="fa-brands fa-twitter" size="2x"/></a> -->
-        <!-- <a href="https://facebook.com/effectai" target="_blank" rel="noopener noreferrer"><font-awesome-icon icon="fa-brands fa-facebook" size="2x"/></a> -->
-        <!-- <a href="https://" target="_blank" rel="noopener noreferrer"><font-awesome-icon icon="fa-brands fa-instagram" size="2x"/></a> -->
-        <!-- <a href="https://medium.com/effect-ai" target="_blank" rel="noopener noreferrer"><font-awesome-icon icon="fa-brands fa-medium" size="2x"/></a> -->
-        <!-- <a href="https://www.youtube.com/@effectai" target="_blank" rel="noopener noreferrer"><font-awesome-icon icon="fa-brands fa-youtube" size="2x"/></a> -->
+        <!-- <a href="https://twitter.com/effectaix" target="_blank" rel="noopener noreferrer"><font-awesome-icon icon="fa-brands fa-twitter" size="xl" style="color: grey"/></a> -->
+        <!-- <a href="https://facebook.com/effectai" target="_blank" rel="noopener noreferrer"><font-awesome-icon icon="fa-brands fa-facebook" size="xl" style="color: grey"/></a> -->
+        <!-- <a href="https://" target="_blank" rel="noopener noreferrer"><font-awesome-icon icon="fa-brands fa-instagram" size="xl" style="color: grey"/></a> -->
+        <!-- <a href="https://medium.com/effect-ai" target="_blank" rel="noopener noreferrer"><font-awesome-icon icon="fa-brands fa-medium" size="xl" style="color: grey"/></a> -->
+        <!-- <a href="https://www.youtube.com/@effectai" target="_blank" rel="noopener noreferrer"><font-awesome-icon icon="fa-brands fa-youtube" size="xl" style="color: grey"/></a> -->
+        <p>© {{ new Date().getFullYear() }} Effect Network</p>
       </div>
       <div class="level-item level-right"></div>
     </div>
@@ -28,6 +30,6 @@ export default {
 
 <style>
 .footer {
-  padding: 0.5rem !important;
+  padding: 15px 15px 15px !important;
 }
 </style>
