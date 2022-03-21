52a6689352c4e26c8e41b6c56819d47a7196e9c1
David Britt <david@effect.ai>
2022-03-21 18:11:02 +0100

    Add get relayer status method

--- plugins/blockchain.js
+++ plugins/blockchain.js
@@ -34,6 +34,7 @@ export default (context, inject) => {
         forceRefreshInterval: null,
         templateRefreshInterval: null,
         eosInfo: null,
+        relayerStatus: null,
         triggerGenerate: false
       }
     },
@@ -93,6 +94,7 @@ export default (context, inject) => {
       updateUserInfo () {
         this.getEfxPrice()
         this.getBlockchainInfo()
+        this.getRelayerStatus()
         if (context.$auth.loggedIn) {
           // console.log('updating user and balance..')
           context.$auth.fetchUser()
@@ -121,6 +123,9 @@ export default (context, inject) => {
           console.error(e)
         }
         this.eosInfo = eosInfo
+      },
+      async getRelayerStatus () {
+        this.relayerStatus = await this.eos.getRelayerStatus().catch(console.error)
       },
       async rememberLogin () {
         const rememberAccount = context.$auth.$storage.getUniversal('rememberAccount')
