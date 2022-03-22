981727f3d90118ce5cd1a0f5c1f1559554162464
David Britt <david@effect.ai>
2022-03-22 12:04:51 +0100

    Use computed properties for relayer values

--- pages/status.vue
+++ pages/status.vue
@@ -15,24 +15,29 @@
       </div>
       <ul>
         <!-- <li v-for="(status, index) in $blockchain.relayerStatus" :key="index.head_block_num">{{ index }}: {{ status }}</li> -->
-        <li> account_name: {{ $blockchain.relayerStatus.account_name }} </li>
+        <li :key="accountNameRelayer"> account_name: {{ accountNameRelayer }} </li>
         <!-- <li> ram_quota: {{ $blockchain.relayerStatus.ram_quota }} </li> -->
         <!-- <li> net_weight: {{ $blockchain.relayerStatus.net_weight }} </li> -->
         <!-- <li> cpu_weight: {{ $blockchain.relayerStatus.cpu_weight }} </li> -->
 
         <li>
-        <div class="has-text-centered">RAM: <strong>{{ percentageRam }}%</strong> | {{ $blockchain.relayerStatus.ram_usage }} / {{ $blockchain.relayerStatus.ram_quota }} </div>
-        <progress class="progress is-link" name="progress" :value="$blockchain.relayerStatus.ram_usage" :max="$blockchain.relayerStatus.ram_quota"></progress>
+          <div class="has-text-centered">
+            RAM: <strong>{{ percentageRam }}%</strong>
+            <br>
+            {{ relayerRamUsage }} / {{ relayerRamQuota }}
+          </div>
+          <progress class="progress is-link" name="progress" :value="relayerRamUsage" :max="relayerRamQuota"></progress>
         </li>
 
         <li>
-        <div class="has-text-centered">NET: <strong>{{ percentageNet }}%</strong> | {{ $blockchain.relayerStatus.net_limit.used }} / {{ $blockchain.relayerStatus.net_limit.max }} </div>
-        <progress class="progress is-link" :value="$blockchain.relayerStatus.net_limit.used" :max="$blockchain.relayerStatus.net_limit.max"></progress>
+        <div class="has-text-centered">
+          NET: <strong>{{ percentageNet }}%</strong></div>
+        <progress class="progress is-link" :value="relayerNetUsage" :max="relayerNetQuota"></progress>
         </li>
 
         <li>
-        <div class="has-text-centered">CPU: <strong>{{ percentageCpu }}%</strong> | {{ $blockchain.relayerStatus.cpu_limit.used }} / {{ $blockchain.relayerStatus.cpu_limit.max }} </div>
-        <progress class="progress is-link" :value="$blockchain.relayerStatus.cpu_limit.used" :max="$blockchain.relayerStatus.cpu_limit.max"></progress>
+        <div class="has-text-centered">CPU: <strong>{{ percentageCpu }}%</strong> | {{ relayerCpuQuota }} / {{ relayerCpuQuota }} </div>
+        <progress class="progress is-link" :value="relayerCpuQuota" :max="relayerCpuQuota"></progress>
         </li>
       </ul>
 
@@ -48,34 +53,54 @@ export default {
     }
   },
   created () {
-
   },
   methods: {
 
   },
   computed: {
+
+    relayer () {
+      return this.$blockchain ? this.$blockchain.relayerStatus : null
+    },
+
+    accountNameRelayer () {
+      return this.$blockchain.relayerStatus ? this.$blockchain.relayerStatus.account_name : '...'
+    },
+
+    relayerRamUsage () {
+      return this.$blockchain.relayerStatus ? this.$blockchain.relayerStatus.ram_usage : 0
+    },
+
+    relayerRamQuota () {
+      return this.$blockchain.relayerStatus ? this.$blockchain.relayerStatus.ram_quota : 0
+    },
+
+    relayerNetUsage () {
+      return this.$blockchain.relayerStatus ? this.$blockchain.relayerStatus.net_limit.used : 0
+    },
+
+    relayerNetQuota () {
+      return this.$blockchain.relayerStatus ? this.$blockchain.relayerStatus.net_limit.max : 0
+    },
+
+    relayerCpuUsage () {
+      return this.$blockchain.relayerStatus ? this.$blockchain.relayerStatus.cpu_limit.used : 0
+    },
+
+    relayerCpuQuota () {
+      return this.$blockchain.relayerStatus ? this.$blockchain.relayerStatus.cpu_limit.max : 0
+    },
+
     percentageRam () {
-      if (this.$blockchain) {
-        return parseInt(this.$blockchain.relayerStatus.ram_usage / this.$blockchain.relayerStatus.ram_quota * 100, 10) ?? 0
-      } else {
-        return 0
-      }
+      return this.$blockchain.relayerStatus ? parseInt(this.$blockchain.relayerStatus.ram_usage / this.$blockchain.relayerStatus.ram_quota * 100, 10) : 0
     },
 
     percentageNet () {
-      if (this.$blockchain) {
-        return parseInt(this.$blockchain.relayerStatus.net_limit.used / this.$blockchain.relayerStatus.net_limit.max * 100, 10) ?? 0
-      } else {
-        return 0
-      }
+      return this.$blockchain.relayerStatus ? parseInt(this.$blockchain.relayerStatus.net_limit.used / this.$blockchain.relayerStatus.net_limit.max * 100, 10) : 0
     },
 
     percentageCpu () {
-      if (this.$blockchain) {
-        return parseInt(this.$blockchain.relayerStatus.cpu_limit.used / this.$blockchain.relayerStatus.cpu_limit.max * 100, 10) ?? 0
-      } else {
-        return 0
-      }
+      return this.$blockchain.relayerStatus ? parseInt(this.$blockchain.relayerStatus.cpu_limit.used / this.$blockchain.relayerStatus.cpu_limit.max * 100, 10) : 0
     }
   }
 
