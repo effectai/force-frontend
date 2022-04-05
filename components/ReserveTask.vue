7c74609dc1c522ae44ff293128e64b4bb9aff83e
Jairzijp <jairzijp123@gmail.com>
2022-04-05 21:20:54 +0200

    wip wip

--- components/ReserveTask.vue
+++ components/ReserveTask.vue
@@ -8,7 +8,7 @@
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
+import { mapGetters, mapState } from 'vuex'
 const retry = require('async-retry')
 
 export default {
@@ -27,15 +27,23 @@ export default {
       loading: false,
       error: null,
       now: 0,
-      reserveInBatch: null
+      reserveInBatch: null,
+      openBatches: []
     }
   },
   computed: {
     ...mapGetters({
-      batchesByCampaignId: 'campaign/batchesByCampaignId'
+      batchesByCampaignId: 'campaign/batchesByCampaignId',
+      submissionsByBatchId: 'campaign/submissionsByBatchId'
+    }),
+    ...mapState({
+      allSubmissionsLoaded: state => state.campaign.allSubmissionsLoaded
     }),
     campaignBatches () {
       return this.batchesByCampaignId(this.campaignId)
+    },
+    submissions () {
+      return this.submissionsByBatchId(this.batchId)
     }
   },
   mounted () {
@@ -44,12 +52,17 @@ export default {
     this.makeReservation()
   },
   methods: {
+    async getSubmissions () {
+      await this.$store.dispatch('campaign/getSubmissions')
+    },
     updateNow () {
       this.now = parseInt((Date.now() / 1000).toFixed(0))
     },
     async makeReservation () {
       try {
+        await this.getSubmissions()
         // if there's no batch specified, check in all the batches if there's still available tasks
+        // TODO: check all the batches, could be there are still tasks available, but not for the current worker
         if (!this.batch) {
           for (const batch of this.campaignBatches) {
             if (batch.num_tasks * batch.repetitions > batch.tasks_done) {
@@ -63,13 +76,21 @@ export default {
           const batch = this.batch ? this.batch : this.reserveInBatch
           await this.$store.dispatch('campaign/getBatchTasks', batch)
           this.loading = true
-          const rs = await this.$blockchain.getSubmissionsAndReservationsForBatch(batch.batch_id)
-          const reservations = rs.filter((r) => {
-            return !r.data || !r.data.length
-          })
-          const submissions = rs.filter((s) => {
-            return s.data || s.data.length
-          })
+          this.batchId = batch.batch_id
+          const rs = { ...this.submissions }
+          const reservations = []
+          const submissions = []
+          for (const r of Object.values(rs)) {
+            if (!r.data || !r.data.length) {
+              reservations.push(r)
+            }
+          }
+
+          for (const r of Object.values(rs)) {
+            if (r.data || r.data.length) {
+              submissions.push(r)
+            }
+          }
 
           // get a reservation for the user
           // could be an active reservation of the user, or an expired/released reservation in the batch
@@ -80,7 +101,6 @@ export default {
             if (rvObj.isExpired) {
               // (re) claim expired task
               const result = await this.$blockchain.claimExpiredTask(rvObj.reservation.id)
-              console.log('resuklt', result)
               this.$store.dispatch('transaction/addTransaction', result)
               rvObj = await this.findReservation(rvObj, batch)
             } else if (rvObj.isReleased) {
@@ -95,54 +115,11 @@ export default {
             }
           } else {
             // User doesn't have reservation yet, so let's make one!
-            let taskIndex
-
-            // First go through the submissions and get all the indexes of the tasks that are done
-            const indexes = []
-            const userIndexes = []
-
-            const treeLeaves = await this.$blockchain.getTreeLeaves(batch.campaign_id, batch.id, batch.tasks)
-            for (const sub of submissions) {
-              const index = await this.$blockchain.getTaskIndexFromLeaf(batch.campaign_id, batch.id, sub.leaf_hash, batch.tasks, treeLeaves)
-              indexes.push(index)
-              if (sub.account_id === this.$auth.user.vAccountRows[0].id) {
-                userIndexes.push(index)
-              }
-            }
-
-            if (indexes.length > 0) {
-              // create object, which holds the count of the task indexes in the submissions
-              const indexesCount = {}
-              for (let i = 0; i < batch.num_tasks; i++) {
-                indexesCount[i] = 0
-              }
-              for (const num of indexes) {
-                indexesCount[num] = indexesCount[num] ? indexesCount[num] + 1 : 1
-              }
-
-              // grab the first available index, that the user hasn't done yet
-              const availableIndex = Object.keys(indexesCount).find(key => indexesCount[key] < batch.repetitions && !this.didWorkerDoTask(userIndexes, key))
-              taskIndex = availableIndex ? parseInt(availableIndex) : null
-              console.log('Found an available task index: ', taskIndex)
-            } else {
-              // no submissions yet in batch
-              taskIndex = 0
-            }
-
-            // if the taskIndex is empty, it means that there are no available tasks anymore
-            if (taskIndex !== null) {
-              const result = await this.$blockchain.reserveTask(batch.id, taskIndex, batch.campaign_id, batch.tasks)
-              this.$store.dispatch('transaction/addTransaction', result)
-              // get reservations and see if this user has a reservation
-              rvObj = await this.findReservation(rvObj, batch)
-            } else {
-              this.$router.push('/campaigns/' + batch.campaign_id + '/' + batch.batch_id + '?batchCompleted=1')
-            }
+            rvObj = await this.createNewReservation(batch, submissions, rvObj)
           }
 
           // redirect to reservation
           if (rvObj.reservation) {
-            console.log('batch', batch)
             const taskIndex = await this.$blockchain.getTaskIndexFromLeaf(batch.campaign_id, batch.id, rvObj.reservation.leaf_hash, batch.tasks)
             this.$router.push('/campaigns/' + batch.campaign_id + '/' + batch.batch_id + '/' + taskIndex + '?submissionId=' + rvObj.reservation.id)
           } else {
@@ -157,6 +134,51 @@ export default {
         this.loading = false
         this.$blockchain.handleError(e)
         this.$router.push('/campaigns/' + this.campaignId)
+      }
+    },
+    async createNewReservation (batch, submissions, rvObj) {
+      let taskIndex
+
+      // First go through the submissions and get all the indexes of the tasks that are done
+      const indexes = []
+      const userIndexes = []
+
+      const treeLeaves = await this.$blockchain.getTreeLeaves(batch.campaign_id, batch.id, batch.tasks)
+      for await (const sub of submissions) {
+        const index = await this.$blockchain.getTaskIndexFromLeaf(batch.campaign_id, batch.id, sub.leaf_hash, batch.tasks, treeLeaves)
+        indexes.push(index)
+        if (sub.account_id === this.$auth.user.vAccountRows[0].id) {
+          userIndexes.push(index)
+        }
+      }
+
+      if (indexes.length > 0) {
+        // create object, which holds the count of the task indexes in the submissions
+        const indexesCount = {}
+        for (let i = 0; i < batch.num_tasks; i++) {
+          indexesCount[i] = 0
+        }
+        for (const num of indexes) {
+          indexesCount[num] = indexesCount[num] ? indexesCount[num] + 1 : 1
+        }
+
+        // grab the first available index, that the user hasn't done yet
+        const availableIndex = Object.keys(indexesCount).find(key => indexesCount[key] < batch.repetitions && !this.didWorkerDoTask(userIndexes, key))
+        taskIndex = availableIndex ? parseInt(availableIndex) : null
+        console.log('Found an available task index: ', taskIndex)
+      } else {
+        // no submissions yet in batch
+        taskIndex = 0
+      }
+
+      // if the taskIndex is empty, it means that there are no available tasks anymore
+      if (taskIndex !== null) {
+        const result = await this.$blockchain.reserveTask(batch.id, taskIndex, batch.campaign_id, batch.tasks)
+        this.$store.dispatch('transaction/addTransaction', result)
+        // get reservations and see if this user has a reservation
+        return await this.findReservation(rvObj, batch)
+      } else {
+        this.$router.push('/campaigns/' + batch.campaign_id + '/' + batch.batch_id + '?batchCompleted=1')
       }
     },
     didWorkerDoTask (userIndexes, key) {
@@ -188,8 +210,7 @@ export default {
       return await retry(async () => {
         const rvs = await this.$blockchain.getTaskReservationsForBatch(batch.batch_id)
         rvObj = await this.getReservationForUser(rvs)
-        console.log('deze gevonden', rvObj)
-        if (!rvObj.reservation || rvObj.isReleased) {
+        if (!rvObj.reservation || rvObj.isReleased || rvObj.isExpired) {
           throw new Error('Reservation not found')
         } else {
           return rvObj
