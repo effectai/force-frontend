82ae07f72804e5316ee1d2da3c035be997b370f0
David Britt <david@effect.ai>
2022-04-21 16:08:43 +0200

    Add qualifications page

---
+++ pages/qualifications/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <section class="section">
+    <div class="container">
+      <h1 class="title mt-5">
+        Qualifications
+      </h1>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
