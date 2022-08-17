28a47eb7e5c278c37bad97feaec5c5f18fdb34c3
djmbritt <david@effect.ai>
2022-08-17 10:23:42 +0200

    Add user has quali to quali page

--- pages/qualifications/_id/index.vue
+++ pages/qualifications/_id/index.vue
@@ -60,6 +60,15 @@
               <div class="columns ">
                 <div class="column is-full">
                   <div class="block">
+                    <div v-if="userHasQuali">
+                      <strong>✅ - You have this Qualification</strong>
+                    </div>
+                    <div v-else>
+                      <strong>❌ - You do not have this Qualification</strong>
+                    </div>
+                  </div>
+                  <hr>
+                  <div class="block">
                     Qualifier Task
                     <br>
                     <div v-if="singleQualification.info.campaignid" class="blockchain-address">
@@ -126,11 +135,20 @@ export default {
   computed: {
     ...mapState({
       qualifications: state => state.qualification.qualifications,
-      loading: state => !state.qualification.allQualificationsLoaded
+      loading: state => !state.qualification.allQualificationsLoaded,
+      assignedQualifications: state => state.qualification.assignedQualifications,
+      allAssignedQualificationsLoaded: state => state.qualification.allAssignedQualificationsLoaded
     }),
     singleQualification () {
       if (!this.qualifications) { return }
       return this.qualifications.find(el => el.id === this.id)
+    },
+    userHasQuali () {
+      if (!this.singleQualification && !this.allAssignedQualificationsLoaded) {
+        return null
+      } else {
+        return this.assignedQualifications.some(el => el.id === this.id)
+      }
     }
   },
   watch: {
