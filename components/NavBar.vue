f9c5a4899f88cf32ab90d938cf609d478455d154
Laurens Verspeek <l.verspeek@gmail.com>
2022-12-20 17:50:17 +0100

    small fix

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -291,12 +291,11 @@ export default {
       try {
         const result = await this.$blockchain.payout()
         this.$store.dispatch('transaction/addTransaction', result)
-        this.$store.dispatch('pendingPayout/loadPendingPayouts')
+        await this.$store.dispatch('pendingPayout/loadPendingPayouts')
         this.transactionUrl = `${this.$blockchain.sdk.config.eosExplorerUrl}/transaction/${result.transaction_id}`
         this.successTitle = 'Payout Completed'
         this.successMessage = 'All your available pending payouts have been completed and are added to your Effect account'
       } catch (error) {
-        this.loading = false
         this.$blockchain.handleError(error)
       }
       this.loading = false
