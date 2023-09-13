0f21fd8e79362720bdcc4bf9cc67f7bb12254e25
David Britt <david@effect.ai>
2023-09-13 22:54:40 +0200

    Setup nuxtPage and layouts

---
+++ layouts/default.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <nav-bar />
+        <section class="">
+            <div class="">
+                <slot />
+            </div>
+        </section>
+        <Footer />
+    </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="container mx-auto my-10">
+        <h1>
+            Effect Network
+        </h1>
+
+        <button @click="login()">Login</button>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+const login = async () => {
+    const { $sessionKit } = useNuxtApp()
+    await $sessionKit.login()
+}
+
+</script>
+
+
+<style >
+
+</style>
