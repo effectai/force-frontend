1c50394484d8a50e2563074e3bf030c038b81993
David Britt <david@effect.ai>
2023-09-16 18:30:29 +0200

    Use composition api for index vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,21 @@
 <template>
     <div class="container mx-auto my-10">
         <p>
-            Effect Network is the largerst decentralized network for human annoted Machine Learning data 
+            Effect Network is the largerst decentralized network for human annoted Machine Learning data
         </p>
+        <div v-if="user">
+            <p>
+                You are logged in as {{ user }}
+            </p>
+        </div>
     </div>
 </template>
 
 <script setup lang="ts">
+const effectClient = useEffectClient().effectClient
 
+const user = ref(() => effectClient?.session?.actor?.toJSON())
 </script>
-
 
 <style >
 
