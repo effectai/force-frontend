f5d339f8908238912c926a2bd9f0f55708279027
David Britt <david@effect.ai>
2023-09-16 18:30:41 +0200

    rm comments from effect plugin

--- plugins/effect.client.ts
+++ plugins/effect.client.ts
@@ -2,15 +2,9 @@ import { Client } from '@effectai/effect-js/dist/'
 
 export default defineNuxtPlugin(() => {
     const effectClient: Client = new Client('jungle4')
-    console.log('effect.client.ts', effectClient)
-
-    // NOTE: Here the methods are availabe.
-    // effectClient.loginWithSession(new Session())
-
     return {
         provide: {
             effectClient,
-
         },
         
     }
