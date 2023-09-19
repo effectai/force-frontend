2e65eac6fa704143c9d02829b4d0799113d02e87
David Britt <david@effect.ai>
2023-09-19 14:18:44 +0200

    Use effect composable

--- composables/effect.ts
+++ composables/effect.ts
@@ -1,5 +1,41 @@
-import { Client } from "@effectai/effect-js";
+import { Client, } from "@effectai/effect-js/dist/";
+import { VAccount,  } from "@effectai/effect-js/dist/types/user";
 
-const effectClient = reactive(new Client("jungle4"));
+const { sessionKit } = useSessionKit()
+const { fetch } = sessionKit
 
-export const useEffectClient = () => ({ effectClient })
+const effectClient = reactive(new Client("jungle4", { fetch }));
+
+const userLoggedIn: Ref<boolean> = ref(false)
+const userAccount: Ref<VAccount | null> = ref(null)
+const userName: Ref<string> = ref('')
+const userPermission: Ref<string> = ref('')
+const efxPrice: Ref<number> = ref(0)
+
+const connectWallet = async () => {
+    console.debug('connectWallet')
+    const { session } = await sessionKit.login()
+    effectClient.loginWithSession(session)
+    // efxPrice.value = await effectClient.efx.getEfxPrice()
+    userAccount.value = await effectClient.vaccount.get()
+    userName.value = effectClient?.session.actor.toString()
+    userPermission.value = await effectClient?.session?.permission.toString()
+    userLoggedIn.value = true
+}
+
+const disconnectWallet = async () => {
+    console.debug('disconnectWallet')
+    await sessionKit.logout()
+    userLoggedIn.value = false
+    // effectClient.logout() // TODO: Should we add this to the effect-js library?
+}
+
+export const useEffectClient = () => ({
+    effectClient,
+    connectWallet,
+    disconnectWallet,
+    userLoggedIn,
+    userName,
+    userPermission,
+    userAccount,
+ })
