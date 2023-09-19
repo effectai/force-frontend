01653e5d133461576a39787e7eae0e647031b782
David Britt <david@effect.ai>
2023-09-19 14:18:05 +0200

    update port and host for nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,8 +3,9 @@ export default defineNuxtConfig({
   ssr: false,
   devtools: { enabled: true },
   devServer: {
-    port: 3000,
-    host: '0.0.0.0',
+    port: 3030,
+    // host: '0.0.0.0',
+    host: 'localhost',
   },
   modules: [
     '@nuxtjs/tailwindcss',
