5670b8f711584972233ca5d2d48c8985827ded15
David Britt <david@effect.ai>
2023-09-19 15:49:33 +0200

    Add hero section to home page

--- pages/index.vue
+++ pages/index.vue
@@ -1,33 +1,35 @@
 <template>
     <div class="container mx-auto my-10">
-        <p>
-            Effect Network is the largerst decentralized network for human annoted Machine Learning data
-        </p>
         <div v-if="userLoggedIn">
-            <p>
-                You are logged in as {{ userName }}
-                <code>
-                    {{ userAccount }}
-                </code>
-                <hr>
-                <code>
-                    {{ campaigns }}
-                </code>
-            </p>
+            <CampaignList />
+        </div>
+        <div v-else>
+            <div class="hero min-h-screen"
+                style="background-image: url(https://daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.jpg);">
+                <div class="hero-overlay bg-opacity-60"></div>
+                <div class="hero-content text-center text-neutral-content">
+                    <div class="max-w-md">
+                        <h1 class="mb-5 text-5xl font-bold">Effect Network</h1>
+                        <p class="mb-5">
+                            Effect Network is the largerst decentralized network for human annoted Machine Learning data
+                        </p>
+                        <button @click="connectWallet" class="btn btn-primary">Get Started</button>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </template>
 
 <script setup lang="ts">
-const { 
+const {
     userLoggedIn,
     userName,
     userAccount,
-    campaigns
- } = useEffectClient()
+    campaigns,
+    connectWallet,
+} = useEffectClient()
 
 </script>
 
-<style >
-
-</style>
+<style ></style>
