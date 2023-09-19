f7c2f987e64bd90fb4bff29176a9ff4a6a554713
David Britt <david@effect.ai>
2023-09-20 00:07:58 +0200

    Persist session

--- composables/effect.ts
+++ composables/effect.ts
@@ -1,33 +1,33 @@
 import { Client, } from "@effectai/effect-js/dist/";
 import { VAccount, Campaign, Reservation } from "@effectai/effect-js/dist/types/index";
+import { Session } from "@wharfkit/session";
 
 const { sessionKit } = useSessionKit()
 const { fetch } = sessionKit
 
 const effectClient = reactive(new Client("jungle4", { fetch }));
 
-const userLoggedIn: Ref<boolean> = ref(false)
-const userAccount: Ref<VAccount | null> = ref(null)
-const userName: Ref<string> = ref('')
-const userPermission: Ref<string> = ref('')
-const efxPrice: Ref<number> = ref(0)
-const reservation: Ref<Reservation | null> = ref(null)
-const campaigns: Ref<Campaign[]> = ref([])
-const campaignsData: Ref<Campaign[]> = ref([])
+const userLoggedIn   : Ref<boolean>            = ref(false)
+const userAccount    : Ref<VAccount | null>    = ref(null)
+const userName       : Ref<string>             = ref('')
+const userPermission : Ref<string>             = ref('')
+const efxPrice       : Ref<number>             = ref(0)
+const reservation    : Ref<Reservation | null> = ref(null)
+// const campaigns      : Ref<Campaign[]>         = ref([])
+const campaignList  : Ref<Campaign[]>         = ref([])
 
-const connectWallet = async () => {
+const connectWallet = async (session?: Session) => {
     // Login
-    const restoreSession = await sessionKit.restore()
-    if (restoreSession) {
-        effectClient.loginWithSession(restoreSession)
-    } else {
-        const { session } = await sessionKit.login()
+    if (session) {
         effectClient.loginWithSession(session)
+    } else {
+        const { session: newSession } = await sessionKit.login()
+        effectClient.loginWithSession(newSession)
     }
     
-    // Retrieve reservation
-    campaigns.value = await effectClient.tasks.getAllCampaigns()
-    campaignsData.value = await Promise.all(campaigns.value.map(getCampaignContent))
+    // Retrieve Campaigns
+    const allCampaigns = await effectClient.tasks.getAllCampaigns()
+    campaignList.value = await Promise.all(allCampaigns.map(getCampaignContent))
 
     // Retrieve user Data
     // efxPrice.value = await effectClient.efx.getEfxPrice()
@@ -53,7 +53,10 @@ const disconnectWallet = async () => {
 }
 
 // Uncomment to persist session
-// await connectWallet()
+const restoreSession = await sessionKit.restore()
+if (restoreSession) {
+    await connectWallet(restoreSession)
+}
 
 export const useEffectClient = () => ({
     effectClient,
@@ -65,6 +68,5 @@ export const useEffectClient = () => ({
     userAccount,
     efxPrice,
     reservation,
-    campaigns,
-    campaignsData,
+    campaignList,
  })
