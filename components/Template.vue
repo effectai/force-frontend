3e099cd7563cdb00227fc5c2d65caa5315cb2b76
David Britt <david@effect.ai>
2023-09-25 18:26:21 +0200

    Add template render component

---
+++ components/Template.vue
@@ -0,0 +1,111 @@
+<template>
+    <div
+        class="template-wrapper"
+        style="width: 100%; height: 100%; border: none;"
+    >
+        <div v-if="loading" class="loading-text subtitle">
+            Loading
+        </div>
+        <!-- TODO: add polyfill for srcdoc or move to different URL and use src -->
+        <iframe
+            id="mediaFrame"
+            ref="mediaFrame"
+            src="https://effectai.github.io/force-frontend/template"
+            style="width: 100%; height: 100%; border: none;"
+            name="mediaFrame"
+            sandbox="allow-scripts allow-modals allow-downloads allow-forms allow-popups allow-popups-to-escape-sandbox allow-pointer-lock allow-same-origin"
+            allow="geolocation; microphone; camera; autoplay; fullscreen"
+            allowFullScreen
+            @load="mediaFrameLoaded"
+        />
+
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        html: {
+            type: String,
+            default: null
+        }
+    },
+    data() {
+        return {
+            alternativeFrontendUrl: process.env.NUXT_ENV_ALTERNATIVE_FRONTEND,
+            loading: true,
+            iframeLoaded: false
+        }
+    },
+    watch: {
+        html(html) {
+            const frame = document.getElementById('mediaFrame')
+            frame.contentWindow.postMessage({ task: 'template', value: html }, '*')
+        }
+    },
+    mounted() {
+        const frame = document.getElementById('mediaFrame')
+        frame.addEventListener('load', this.frameIsLoaded)
+        document.addEventListener('keydown', this.sendKeyEvents)
+    },
+    created() {
+        console.debug('Template created', this.html)
+        // eslint-disable-next-line nuxt/no-globals-in-created
+        window.addEventListener('message', this.communicateWithIframe)
+    },
+    beforeDestroy() {
+        window.removeEventListener('message', this.communicateWithIframe)
+        document.removeEventListener('keydown', this.sendKeyEvents)
+    },
+    methods: {
+        // helps check when iframe is fully rendered
+        frameIsLoaded() {
+            this.iframeLoaded = true
+        },
+        mediaFrameLoaded() {
+            this.loading = false
+            const frame = document.getElementById('mediaFrame')
+
+            frame.contentWindow.postMessage({ task: 'template', value: this.html }, '*')
+            this.$emit('templateLoaded')
+        },
+        sendKeyEvents(e) {
+            const frame = document.getElementById('mediaFrame')
+            if (frame) {
+                frame.contentWindow.postMessage({ task: 'key', key: e.key }, '*')
+            }
+        },
+        communicateWithIframe(event) {
+            // TODO: check if sender of event is template iframe
+            const data = event.data
+            switch (data.task) { // postMessage tasks
+                case 'setHeight':
+                    // eslint-disable-next-line no-case-declarations
+                    const frame = document.getElementById('mediaFrame')
+                    frame.height = (data.height) + 'px'
+                    break
+                case 'submit':
+                    // TaskContent handles submit
+                    this.$emit('submit', data.values)
+                    break
+                case 'results':
+                    // eslint-disable-next-line no-case-declarations
+                    const mediaFrame = document.getElementById('mediaFrame')
+                    mediaFrame.contentWindow.postMessage(data, '*')
+                    break
+            }
+        }
+    }
+}
+</script>
+
+<!-- <style lang="scss" scoped>
+.template-wrapper {
+    width: 100%;
+}
+
+#mediaFrame {
+    width: 100%;
+    border: none;
+}
+</style> -->
