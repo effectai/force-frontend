ae1311e44d3e9ac33df74327cece7e11f4d99a20
David Britt <david@effect.ai>
2023-09-25 15:58:46 +0200

    Add connect wallet button

---
+++ components/ConnectWallet.vue
@@ -0,0 +1,13 @@
+<template>
+    <button @click="connectWallet()" class="btn btn-primary">
+        <span :class="{ 'loading': walletConnecting, 'loading-spinner': walletConnecting}"></span>
+        Connect Wallet
+    </button>
+</template>
+
+<script setup lang="ts">
+const {
+    connectWallet,
+    walletConnecting,
+} = useEffectClient()
+</script>
