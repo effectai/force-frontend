9b31ff10e722d352ae1fdf4ada5d29d0a4e22a78
David Britt <david@effect.ai>
2023-10-03 13:45:38 +0200

    render buttons vertically

--- pages/index.vue
+++ pages/index.vue
@@ -13,11 +13,12 @@
                         <p class="mb-5">
                             Effect Network is the largerst decentralized network for human annoted Machine Learning data
                         </p>
-                        <div class="join join-vertical lg:join-horizontal">
-                            <ConnectWallet class="join-item" />
+                        <div class="join join-vertical">
+                            <!-- <ConnectWallet class="join-item" /> -->
                             <a href="https://dao.effect.network" class="btn join-item btn-accent" target="_blank" rel="noopener noreferrer">DAO</a>
                             <a href="https://docs.effect.network" class="btn join-item btn-secondary" target="_blank" rel="noopener noreferrer">Documentation</a>
                             <a href="https://discord.gg/effectnetwork" class="btn join-item btn-info" target="_blank" rel="noopener noreferrer">Discord</a>
+                            <a href="https://t.me/effectnetwork" class="btn join-item btn-warning" target="_blank" rel="noopener noreferrer">Telegram</a>
                         </div>
                     </div>
                 </div>
