2843153897d46d8a56cc81457ff2e1ba7eb26875
David Britt <david@effect.ai>
2023-10-12 12:37:37 +0200

    Disable connectWallet loading

--- components/ConnectWallet.vue
+++ components/ConnectWallet.vue
@@ -1,8 +1,12 @@
 <template>
-    <button @click="connectWallet()" class="btn btn-primary">
-        <span :class="{ 'loading': walletConnecting, 'loading-spinner': walletConnecting}"></span>
+    <button v-if="true" @click="connectWallet()" class="btn btn-primary">
         Connect Wallet
     </button>
+    <!-- <button v-else class="btn btn-outline btn-disabled">
+        <span :class="{ 'loading': walletConnecting, 'loading-spinner': walletConnecting}"></span>
+        <span class="loading loading-spinner"></span>
+        Connecting
+    </button> -->
 </template>
 
 <script setup lang="ts">
