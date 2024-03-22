52860cd6f4ad121bb3d098065f460269c834f6bb
Jeffrey <jeffrey@1nteger.nl>
2024-03-22 08:52:03 +0100

    forward to homepage if not logged in

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -1,9 +1,8 @@
 const { isLoggedIn } = useEffectClient();
 
 export default defineNuxtRouteMiddleware((to, from) => {
-    console.log(isLoggedIn.value);
     // If the user is not logged in, redirect to home page
     if (!isLoggedIn.value) {
-        return navigateTo("/login");
+        return navigateTo("/");
     }
 });
